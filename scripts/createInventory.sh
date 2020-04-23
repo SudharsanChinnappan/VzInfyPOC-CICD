30aa82e6310dd8c937fb88a9a0c6c591ede5d5b1
SudharsanChinnappan <chinnappan.sudharsan@gmail.com>
2020-04-23 08:48:23 +0530

    Update createInventory.sh

--- scripts/createInventory.sh
+++ scripts/createInventory.sh
@@ -1,7 +1,7 @@
 echo '[kubernetes-master]' > inventory
-aws ec2 describe-instances --region us-east-1 --filters "Name=tag:ApplicationName,Values=VzInfyPOC-MyWebApp" "Name=tag:Role,Values=Master"--query 'Reservations[*].Instances[*].[PublicIpAddress]' | awk -F "\"" '{print $2}' | sed '/^$/d'  >> inventory
+aws ec2 describe-instances --region us-east-1 --filters "Name=tag:ApplicationName,Values=VzInfyPOC-MyWebApp","Name=tag:Role,Values=Master" --query 'Reservations[*].Instances[*].[PublicIpAddress]' | awk -F "\"" '{print $2}' | sed '/^$/d'  >> inventory
 echo '[kubernetes-worker]' >> inventory
-aws ec2 describe-instances --region us-east-1 --filters "Name=tag:ApplicationName,Values=VzInfyPOC-MyWebApp" "Name=tag:Role,Values=Worker"--query 'Reservations[*].Instances[*].[PublicIpAddress]' | awk -F "\"" '{print $2}' | sed '/^$/d'  >> inventory
+aws ec2 describe-instances --region us-east-1 --filters "Name=tag:ApplicationName,Values=VzInfyPOC-MyWebApp","Name=tag:Role,Values=Worker" --query 'Reservations[*].Instances[*].[PublicIpAddress]' | awk -F "\"" '{print $2}' | sed '/^$/d'  >> inventory
 echo '[cluster:children]' >> inventory
 echo 'kubernetes-master' >> inventory
 echo 'kubernetes-worker' >> inventory
