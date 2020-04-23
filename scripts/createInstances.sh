12c4682010f05b0764fc8d577941697ea72e0daf
SudharsanChinnappan <chinnappan.sudharsan@gmail.com>
2020-04-23 08:30:56 +0530

    correction

--- scripts/createInstances.sh
+++ scripts/createInstances.sh
@@ -5,7 +5,8 @@ aws cloudformation create-stack \
 --template-body file://cf-template/createSimpleWebApp.yml \
 --parameters ParameterKey=NetworkStackName,ParameterValue=VzInfyPOC-Network \
 ParameterKey=ApplicationName,ParameterValue=VzInfyPOC-MyWebApp \
-ParameterKey=InstanceType,ParameterValue=t2.medium \
+ParameterKey=MasterInstanceType,ParameterValue=t2.medium \
+ParameterKey=WorkerInstanceType,ParameterValue=t2.micro \
 ParameterKey=KeyName,ParameterValue=ansiblekey \
 ParameterKey=Environment,ParameterValue=DEV
 aws cloudformation wait stack-create-complete --stack-name VzInfyPOC-MyWebApp --region us-east-1
