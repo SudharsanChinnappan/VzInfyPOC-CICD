83ddf51623a6056dbc912804bdcbd3c483b4e22d
SudharsanChinnappan <chinnappan.sudharsan@gmail.com>
2020-05-03 09:20:57 +0530

    Update install_helm.sh

--- scripts/install_helm.sh
+++ scripts/install_helm.sh
@@ -2,5 +2,5 @@
 wget  ‘https://get.helm.sh/helm-v3.2.0-linux-amd64.tar.gz’
 tar -xvf helm-v3.2.0-linux-amd64.tar.gz
 sudo cp linux-amd64/helm /bin/
-helm repo add helm https://sudharsanc-simplewebappdocker.jfrog.io/artifactory/helm --username admin --password AP6we4X4QBTAVLsE4QNexyc6eFK
+helm repo add helm https://csudharsan.jfrog.io/artifactory/helm --username admin --password AP5sHFBRdT4tZ1ui1z1JWi6UbNU
 helm repo update
